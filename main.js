55222d6c390f425e8c9609b63bd8a9909efaa391
144234 <78003838+144234@users.noreply.github.com>
2022-12-01 18:16:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 function preload(){
-}
  
+}
 function setup(){
-   canvas= createCanvas(640, 480);
-   canvas.position(110, 250);
+   canvas = createCanvas(300, 300);
+   canvas.center();
    video = createCapture(VIDEO);
+   viseo.size(300, 300);
+   video.hide();
  
-   tint_color ="";
+   poseNet = ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotPoses);
 }
- 
+function modelLoaded(){
+   console.log('PoseNet Is Initialized');
+}
+function gotPoses (results)
+{
+   if(results.length > 0)
+   {
+       console.log(results);
+       console.log("nose x =" + results[0].pose.nose.x);
+       console.log("nose y =" + results[0].pose.nose.y);
+   }
+}
 function draw(){
-   
+ Image(video, 0, 0, 300, 300);
 }
- 
 function take_snapshot(){
    SVGFEFuncAElement('myFilterImage.png');
- 
 }
+
